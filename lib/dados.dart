975b1e49d6a4b5a0c4d1ce84cdf10617c71eabbc
Mauricio-Junior-Dev <mauriciobgsp@gmail.com>
2026-09-04 12:07:13 -0300

    desafio de mudança de estado setState

--- lib/dados.dart
+++ lib/dados.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
-class Dados extends StatelessWidget {
+class Dados extends StatefulWidget {
+  @override
+  _DadosState createState() => _DadosState();
+}
+
+class _DadosState extends State<Dados> {
+  var numeroDadoEsquerdo = 1;
+  var numeroDadoDireito = 2;
+
   @override
   Widget build(BuildContext context) {
-    var numeroDadoEsquerdo = 2;
-
-    
     return MaterialApp(
       title: 'Dados',
       home: Scaffold(
@@ -23,7 +28,15 @@ class Dados extends StatelessWidget {
                 child: TextButton(
                   onPressed: () {
                     // Ação quando o botão for pressionado
-                    print('Botão 1 pressionado');
+                    setState(() {
+                    numeroDadoEsquerdo += 1;
+                    if (numeroDadoEsquerdo > 6) {
+                      numeroDadoEsquerdo = 1;
+
+                    }else if (numeroDadoEsquerdo < 1) {
+                      numeroDadoEsquerdo = 6;
+                    }
+                  });
                   },
                   child:
                       Image.asset('assets/dados/dado$numeroDadoEsquerdo.png'),
@@ -33,9 +46,11 @@ class Dados extends StatelessWidget {
                 child: TextButton(
                   onPressed: () {
                     // Ação quando o botão for pressionado
-                    print('Botão 2 pressionado');
+                    setState(() {
+                      numeroDadoDireito = (numeroDadoDireito % 6) + 1;
+                    });
                   },
-                  child: Image.asset('assets/dados/dado2.png'),
+                  child: Image.asset('assets/dados/dado$numeroDadoDireito.png'),
                 ),
               ),
             ],
