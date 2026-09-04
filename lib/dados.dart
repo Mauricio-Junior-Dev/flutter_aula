ef665cfc9c92ac20170586eb619abc7573e9a1df
MauMau221 <mauriciobgsp@gmail.com>
2026-09-04 00:46:38 -0300

    aula 26 add variavel em url de imagem

--- lib/dados.dart
+++ lib/dados.dart
@@ -3,14 +3,44 @@ import 'package:flutter/material.dart';
 class Dados extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var numeroDadoEsquerdo = 2;
+
+    
     return MaterialApp(
       title: 'Dados',
       home: Scaffold(
-        body: Container(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [Image.asset('assets/dados/dado1.png'),
-                  Image.asset('assets/dados/dado2.png'),])),
+        backgroundColor: Colors.teal.shade700,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Dados'),
+          ),
+          backgroundColor: Colors.teal.shade900,
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    // Ação quando o botão for pressionado
+                    print('Botão 1 pressionado');
+                  },
+                  child:
+                      Image.asset('assets/dados/dado$numeroDadoEsquerdo.png'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    // Ação quando o botão for pressionado
+                    print('Botão 2 pressionado');
+                  },
+                  child: Image.asset('assets/dados/dado2.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
